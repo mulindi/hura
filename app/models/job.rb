173a677a0e8a0c6f23ab269f36c8727856f659cd
Daniel M. Matongo <m_matongo@icloud.com>
2022-03-06 09:54:37 +0000

    update model relations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,16 @@
 class Job < ApplicationRecord
+  belongs_to :user
+
+  JOB_TYPES = [
+    'Full-time',
+    'Part-time',
+    'Contract',
+    'Internship',
+    'Freelance',
+    'Volunteer',
+    'Temporary',
+    'Other'
+  ]
+
+  validates :title, presence: true
 end
