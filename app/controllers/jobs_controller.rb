b238954187ca5389ae7214f195c0bdbf67187daf
Daniel M. Matongo <m_matongo@icloud.com>
2022-03-06 10:03:47 +0000

    add main controller logic

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,51 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    if (params.has_key?(:job_type))
+      @jobs = Job.where(job_type: params[:job_type]).order("created_at DESC")
+    else
+      @jobs = Job.all.order("created_at DESC")
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @job = current_user.jobs.build
+  end
+
+  def create
+  end
+
+  def update
+    respond_to do |format|
+      if @job.update(job_params)
+        format.html { redirect_to @job, notice: 'Job was successfully updated.' }
+        format.json { render :show, status: :ok, location: @job }
+      else
+        format.html { render :edit }
+        format.json { render json: @job.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @job.destroy
+    respond_to do |format|
+      format.html { redirect_to jobs_url, notice: 'Job was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description, :company, :url, :job_type, :remote, :apply_url, :avatar)
+  end
 end
